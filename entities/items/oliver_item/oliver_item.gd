e71b311054a244849979b3054585181aa50db9b6
Augustus Mapes <amapes10@ivytech.edu>
2026-09-01 14:42:39 -0400

    v0.1.1 Olivar WIP

--- entities/items/oliver_item/oliver_item.gd
+++ entities/items/oliver_item/oliver_item.gd
@@ -8,6 +8,7 @@ var movement_marker : Node
 @onready var detection_range = $detection_range
 @onready var timer = $Cooldown
 @onready var tooltip = $tooltip
+@onready var camera = $Camera2D
 @export var projectile_scene : PackedScene
 var has_owner = false
 var can_teleport = false
@@ -15,9 +16,11 @@ var debug = false
 var SPEED = 100.0
 var team
 var cooldown = 1
-var follow_dist = 50
+var follow_dist = 75
 var teleport_dist = 500
 var move_target : Node
+var modes = ["Sentry","AI","Player Control"]
+var mode = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -27,87 +30,116 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
 	if has_owner:
-		# gettitng targets
-		var targets : Array
-		for area in detection_range.get_overlapping_areas():
-			if area.is_in_group("targetable"):
-				targets.append(area)
-				if debug:
-					print(area.name + " area")
-		for body in detection_range.get_overlapping_bodies():
-			if body.is_in_group("targetable"):
-				targets.append(body)
-				if debug:
-					print(body.name + " body")
+		if modes[mode - 1] == "Sentry":
+			Sentry_mode()
+		elif modes[mode - 1] == "AI":
+			AI_mode()
+		elif modes[mode - 1] == "Player Control":
+			Player_Ctrl()
 		
-		var closest_target : Node
-		var target_dist : float
-		closest_target = null
-		target_dist = 1000
+func Sentry_mode():
+	attack()
+
+func AI_mode():
+	# gettitng targets
+	var targets : Array
+	for area in detection_range.get_overlapping_areas():
+		if area.is_in_group("targetable"):
+			targets.append(area)
+			if debug:
+				print(area.name + " area")
+	for body in detection_range.get_overlapping_bodies():
+		if body.is_in_group("targetable"):
+			targets.append(body)
+			if debug:
+				print(body.name + " body")
+		
+	var closest_target : Node
+	var target_dist : float
+	closest_target = null
+	target_dist = 1000
+	for target in targets:
+		if target.is_in_group("interactable"):
+			if global_position.distance_to(target.global_position) < target_dist:
+				closest_target = target
+				target_dist = global_position.distance_to(target.global_position)
+	move_target = closest_target
+	can_teleport = false
+	if closest_target != null and debug:
+		print("The closest target is: " + closest_target.name + " " + str(target_dist))
+	
+	if debug:
+		print("List Start:")
 		for target in targets:
-			if target.is_in_group("interactable"):
-				if global_position.distance_to(target.global_position) < target_dist:
-					closest_target = target
-					target_dist = global_position.distance_to(target.global_position)
-		move_target = closest_target
-		can_teleport = false
-		if closest_target != null and debug:
-			print("The closest target is: " + closest_target.name + " " + str(target_dist))
+			print(target.name + " distance: " + str(global_position.distance_to(target.global_position)))
+	
+	if move_target == null:
+		move_target = movement_marker
+		can_teleport = true
+	
+	var movement_direction = (move_target.global_position - global_position).normalized()
+	
+	var dist = global_position.distance_to(move_target.global_position)
+	
+	if dist > follow_dist and dist < teleport_dist and move_target == movement_marker: # follow logic
+		velocity = movement_direction * SPEED
+	elif dist > teleport_dist and can_teleport: # teleportation logic
+		global_position = movement_marker.global_position
+	elif move_target != movement_marker:
+		velocity = movement_direction * SPEED
+	else:
+		if velocity.x > 1.4:
+			velocity.x -= 1
+		elif velocity.x < -1.4:
+			velocity.x += 1
+		elif velocity.x < 1.5 and velocity.x > -1.5:
+			velocity.x = 0
+		
+		if velocity.y > 4.8:
+			velocity.y -= 1
+		elif velocity.y < -4.8:
+			velocity.y += 1
+		elif velocity.y < 8.5 and velocity.y > -8.5:
+			velocity.y = 0
 		
 		if debug:
-			print("List Start:")
-			for target in targets:
-				print(target.name + " distance: " + str(global_position.distance_to(target.global_position)))
-		
-		if move_target == null:
-			move_target = movement_marker
-			can_teleport = true
-		
-		var movement_direction = (move_target.global_position - global_position).normalized()
-		
-		var dist = global_position.distance_to(move_target.global_position)
-		
-		if dist > follow_dist and dist < teleport_dist and move_target == movement_marker: # follow logic
-			velocity = movement_direction * SPEED
-		elif dist > teleport_dist and can_teleport: # teleportation logic
-			global_position = movement_marker.global_position
-		elif move_target != movement_marker:
-			velocity = movement_direction * SPEED
-		else:
-			if velocity.x > 1.4:
-				velocity.x -= 1
-			elif velocity.x < -1.4:
-				velocity.x += 1
-			elif velocity.x < 1.5 and velocity.x > -1.5:
-				velocity.x = 0
-			
-			if velocity.y > 1.4:
-				velocity.y -= 1
-			elif velocity.y < -1.4:
-				velocity.y += 1
-			elif velocity.y < 1.5 and velocity.y > -1.5:
-				velocity.y = 0
-			
 			print(str(velocity))
 			
-		move_and_slide()
+	move_and_slide()
 	
-		# attack logic
-		if timer.is_stopped() and has_owner:
-			for area in attack_range.get_overlapping_areas():
-				if area.is_in_group("targetable") and (area.team != "neutral" and area.team != team):
-					shoot(area)
-					timer.start(cooldown)
-					break
-				
-		# interact logic
-		for area in interact_range.get_overlapping_areas():
-			if area.is_in_group("interactable") and (area != owner_entity and area != self):
-				area.interact(self)
-		for body in interact_range.get_overlapping_bodies():
-			if body.is_in_group("interactable") and (body != owner_entity and body != self):
-				body.interact(self)
+	attack()
+	interaction()
+	
+func Player_Ctrl():
+	var direction : Vector2
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
+	if direction:
+		velocity = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	move_and_slide()
+	interaction()
+	
+func attack():
+	# attack logic
+	if timer.is_stopped() and has_owner:
+		for area in attack_range.get_overlapping_areas():
+			if area.is_in_group("targetable") and (area.team != "neutral" and area.team != team):
+				shoot(area)
+				timer.start(cooldown)
+				break
 
+func interaction():
+	# interact logic
+	for area in interact_range.get_overlapping_areas():
+		if area.is_in_group("interactable") and (area != owner_entity and area != self):
+			area.interact(self)
+	for body in interact_range.get_overlapping_bodies():
+		if body.is_in_group("interactable") and (body != owner_entity and body != self):
+			body.interact(self)
+				
 func shoot(entity):
 	var projectile = projectile_scene.instantiate() #instance projectile
 	projectile.direction = (entity.global_position - global_position).normalized()
--- player/player.gd
+++ player/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
 @onready var interaction_range = $interaction_range
+@onready var camera = $Camera2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var team = "friendly"
+var lock_movement = false
 var drone : Node
 
 func _ready() -> void:
@@ -21,21 +23,37 @@ func _physics_process(delta: float) -> void:
 				print("interacting with " + body.name)
 				body.interact(self)
 				break
+	# swapping drone logic
+	if Input.is_action_just_pressed("swap_drone_action") and drone != null:
+		if drone.mode < len(drone.modes):
+			drone.mode += 1
+		else:
+			drone.mode = 1
+			
+		if drone.mode == 3:
+			camera.enabled = false
+			drone.camera.enabled = true
+			lock_movement = true
+		else:
+			camera.enabled = true
+			drone.camera.enabled = false
+			lock_movement = false
 	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	if not lock_movement:
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("move_left", "move_right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		move_and_slide()
