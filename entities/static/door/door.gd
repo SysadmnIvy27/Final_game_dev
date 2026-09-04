0c7aa53ad2f53b1c411cf17c55689addb2d5721d
Augustus Mapes <amapes10@ivytech.edu>
2026-09-04 14:42:51 -0400

    v0.1.0 Door WIP

--- entities/static/door/door.gd
+++ entities/static/door/door.gd
@@ -1,12 +1,21 @@
 extends StaticBody2D
 
-var ID = "Door1"
+@onready var door_collision = $CollisionShape2D
+var doors : Array
+var ID = name
+var open = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	for child in get_children():
+		if child.is_in_group("Door"):
+			doors.append(child)
+			child.bound_door = self
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	if open == true:
+		door_collision.disabled = true
+	else:
+		door_collision.disabled = false
--- entities/static/photon_sensor/photon_sensor.gd
+++ entities/static/photon_sensor/photon_sensor.gd
@@ -1,18 +1,21 @@
 extends Area2D
 
 @onready var anim = $AnimatedSprite2D
+var bound_door
 var active = false
+var door
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if active:
 		anim.animation = &"active"
+		bound_door.open = true
 	else:
 		anim.animation = &"inactive"
+		bound_door.open = false
 	
 func on_hit():
 	active = not active
